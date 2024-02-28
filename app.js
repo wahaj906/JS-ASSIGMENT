4856043995ca36a29edd8af79819af80a9100cfd
Wahaj khan <154734596+wahaj906@users.noreply.github.com>
2024-02-29 03:43:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+let no1 = prompt("Enter First No.");
+let no2 = prompt("Enter Second No.");
+
+//string to number
+no1 = Number(no1);
+no2 = Number(no2);
+
+//addition
+let plus = no1 + no2;
+alert("Addition: " + plus);
+console.log("Addition " + plus);
+
+
+let minus = no1 - no2;
+alert("Subtraction: " + minus);
+console.log("Subtraction" + minus);
+
+let multiply = no1 * no2;
+alert("Multiplication: " + multiply);
+console.log("Multiplication" + multiply);
+
+
+let divide = no1 / no2;
+alert("Division: " + divide);
+console.log("Division" + divide);
